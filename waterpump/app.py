e510e36be8daeac2cbd767df1ddb66f30efa6ef0
JustinSwin <101616188+JustinSwin@users.noreply.github.com>
2023-05-22 20:25:42 +0800

    Update app.py

--- waterpump/app.py
+++ waterpump/app.py
@@ -4,15 +4,74 @@ import paho.mqtt.client as paho
 import json
 import mysql.connector
 
+def read_water_node_json(message):
+  # Load JSON data
+  data = json.loads(message)
+  
+  # Define table names
+  table_names = {
+      "thresLog": "water_thres_log",
+      "weatherLog": "weather_log",
+      "pumpLog1": "pump_log1",
+      "pumpLog2": "pump_log2",
+      "pumpLog3": "pump_log3",
+      "moistureLog1": "moisture_log1",
+      "moistureLog2": "moisture_log2",
+      "moistureLog3": "moisture_log3"
+  }
+  
+  cursor = mydb.cursor()
+  
+  # Iterate over each key-value pair in the JSON data
+  for table_name, table_data in data.items():
+      if isinstance(table_data, dict):
+        # For tables with dictionary data (pumpLog1, pumpLog2, pumpLog3)
+        for date, value in table_data.items():
+            # Check if the date already exists in the table
+            query = f"SELECT COUNT(*) FROM {table_names[table_name]} WHERE date = %s"
+            cursor.execute(query, (date,))
+            count = cursor.fetchone()[0]
+
+            if count > 0:
+                # Date exists, update the value
+                query = f"UPDATE {table_names[table_name]} SET daily_ml = %s WHERE date = %s"
+                cursor.execute(query, (int(value)*4*20, date))
+            else:
+                # Date doesn't exist, insert a new row
+                query = f"INSERT INTO {table_names[table_name]} (daily_ml, date) VALUES (%s, %s)"
+                cursor.execute(query, (int(value)*4*20, date))
+  
+      elif isinstance(table_data, list):
+          # For tables with list data (thresLog, weatherLog, moistureLog1, moistureLog2, moistureLog3)
+          for row in table_data:
+            
+            if table_name.startswith("moistureLog"):
+                print(f"Inserting row: {row}")  # Debug print statement
+                # For moistureLog tables, insert a new row
+                query = f"INSERT INTO {table_names[table_name]} (moisture_level, timestamp) VALUES (%s, %s)"
+                cursor.execute(query, (row[0], row[1]))
+            elif table_name == "thresLog":
+                # For thresLog  tables, update the values
+                query = f"UPDATE {table_names[table_name]} SET thres = %s WHERE thres_num = %s"
+                cursor.execute(query, (row[1], row[0]))
+            elif table_name == "weatherLog":
+                # For  weatherLog tables, update the values
+                query = f"UPDATE {table_names[table_name]} SET temperature = %s, humidity = %s, status = %s"
+                cursor.execute(query, (row[0], row[1], row[2]))
+  
+  # Commit the changes and close the cursor
+  mydb.commit()
+  cursor.close()
+
 # Assuming you have established a MySQL connection and have a cursor object
 # mydb = mysql.connector.connect(...)
 # cursor = mydb.cursor()
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="pi",
-    password="pi",
-    database="greenhouse_db"
+  host="localhost",
+  user="pi",
+  password="pi",
+  database="greenhouse_db"
 )
 
 
@@ -25,7 +84,7 @@ broker = "30879eac8d1b42ea9dd2feaf91890eb6.s2.eu.hivemq.cloud"
 port = 8883
 username = "user1"
 password = "Password123"
-topic = "nodes/"
+topic = "nodes/#"
 
 client = paho.Client(client_id="")
 mqtt_client = mqtt.Client(client_id="")
@@ -37,6 +96,7 @@ mqtt_client.connect(broker, port)
 
 # Callback functions
 def on_connect(client, userdata, flags, rc, properties=None):
+    mqtt_client.subscribe(topic,qos=1)
     print("CONNACK received with code %s." % rc)
 
 
@@ -52,63 +112,9 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     message = str(msg.payload.decode("utf-8"))
     receiving_topic = str(msg.topic)
-    if receiving_topic == "nodes/water":
+    if receiving_topic == "nodes/water" and message.startswith('{"thresLog":'):
+        read_water_node_json(message)
         
-        # Load JSON data
-        data = json.loads(message)
-        
-        # Define table names
-        table_names = {
-            "thresLog": "water_thres_log",
-            "weatherLog": "weather_log",
-            "pumpLog1": "pump_log1",
-            "pumpLog2": "pump_log2",
-            "pumpLog3": "pump_log3",
-            "moistureLog1": "moisture_log1",
-            "moistureLog2": "moisture_log2",
-            "moistureLog3": "moisture_log3"
-        }
-        
-        cursor = mydb.cursor()
-        
-        # Iterate over each key-value pair in the JSON data
-        for table_name, table_data in data.items():
-            if isinstance(table_data, dict):
-                # For tables with dictionary data (pumpLog1, pumpLog2, pumpLog3)
-                for date, value in table_data.items():
-                    # Check if the date already exists in the table
-                    query = f"SELECT COUNT(*) FROM {table_names[table_name]} WHERE date = %s"
-                    cursor.execute(query, (date,))
-                    count = cursor.fetchone()[0]
-        
-                    if count > 0:
-                        # Date exists, update the value
-                        query = f"UPDATE {table_names[table_name]} SET daily_ml = %s WHERE date = %s"
-                        cursor.execute(query, (int(value)*4*20, date))
-                    else:
-                        # Date doesn't exist, insert a new row
-                        query = f"INSERT INTO {table_names[table_name]} (daily_ml, date) VALUES (%s, %s)"
-                        cursor.execute(query, (int(value)*4*20, date))
-        
-            elif isinstance(table_data, list):
-                # For tables with list data (thresLog, weatherLog, moistureLog1, moistureLog2, moistureLog3)
-                for row in table_data:
-                    if table_name.startswith("moistureLog"):
-                        # For moistureLog tables, insert a new row
-                        query = f"INSERT INTO {table_names[table_name]} (moisture_level, timestamp) VALUES (%s, %s)"
-                        cursor.execute(query, (row[0], row[1]))
-                    elif table_name == "thresLog":
-                        # For thresLog and weatherLog tables, update the values
-                        query = f"UPDATE {table_names[table_name]} SET thres = %s WHERE thres_num = %s"
-                        cursor.execute(query, (row[1], row[0]))
-                    elif table_name == "weatherLog":
-                        # For thresLog and weatherLog tables, update the values
-                        query = f"UPDATE {table_names[table_name]} SET temperature = %s, humidity = %s, status = %s"
-                        cursor.execute(query, (row[0], row[1], row[2]))
-        
-        # Commit the changes and close the cursor
-        mydb.commit()
-        cursor.close()
 
 
 mqtt_client.on_connect = on_connect
@@ -130,7 +136,20 @@ def publish_message():
     mqtt_client.publish(topic, payload=message)
 
     return "Message sent successfully!"
-
+#json_data = 
+'''
+{
+    "thresLog": [["1", "26.7"], ["2", "26.7"], ["3", "26.7"]],
+    "weatherLog": [["31.57", "71%", "Clouds"]],
+    "pumpLog1": {"2023-05-19": 48, "2023-05-20": 33},
+    "pumpLog2": {"2023-05-19": 2},
+    "pumpLog3": {"2023-05-19": 1},
+    "moistureLog1": [[1006, "2023-05-18T14:39:13"], [1009, "2023-05-18T14:39:19"], [1009, "2023-05-18T14:39:25"], [1009, "2023-05-18T14:39:55"], [1009, "2023-05-18T14:40:01"]],
+    "moistureLog2": [[383, "2023-05-18T14:41:01"], [383, "2023-05-18T14:41:07"], [384, "2023-05-18T14:41:13"], [383, "2023-05-18T14:41:19"], [383, "2023-05-18T14:41:25"]],
+    "moistureLog3": [[339, "2023-05-18T14:41:02"], [339, "2023-05-18T14:41:07"], [338, "2023-05-18T14:41:13"], [337, "2023-05-18T14:41:19"], [337, "2023-05-18T14:41:25"]]
+}
+'''
+#read_water_node_json(json_data)
 
 # def publish_data():
 #     # Define your data publishing logic here
