7451219b3b0b4d4494d7a6a0b2f78cfab54fbffa
GordonL99 <gordonits.ling99@gmail.com>
2023-05-23 23:37:26 +0800

    update

--- cloud/app.py
+++ cloud/app.py
@@ -337,9 +337,8 @@ def TempDataVisualization():
     except my.sql.connector.Error as error:
         print("Failed to retrieve data from MySQL: {}".format(error))
         tempAs = []
-        print(tempAs)
-        return jsonify({'tempA' : tempAs})
-    return render_template('data_temp.html', tempAs=tempAs)
+        return render_template('data_temp.html', tempAs=tempAs)
+    return jsonify({'tempA' : tempAs})
 
 @app.route('/data-visualization')
 def datavisualization():
