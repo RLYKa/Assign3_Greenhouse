76b5898655980e5b78eafe7cd38b8c84389561fb
RLayk <101616057+RLYKa@users.noreply.github.com>
2023-05-24 09:26:50 +0800

    Change

--- light/app.py
+++ light/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import paho.mqtt.client as mqtt
-import ast
 
 app = Flask(__name__)
 broker = "30879eac8d1b42ea9dd2feaf91890eb6.s2.eu.hivemq.cloud"
@@ -14,39 +13,22 @@ mqtt_client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLSv1_2)
 
 mqtt_client.connect(broker, port)
 
-
-latest_status = {
-    "LED 1": {"state": 0, "threshold": 0},
-    "LED 2": {"state": 0, "threshold": 0},
-    "LED 3": {"state": 0, "threshold": 0},
-}
-
 TOPIC_STATUS = 'nodes/status'
 TOPIC_THRESHOLD = 'nodes/threshold'
 TOPIC_LED = 'nodes/ledControl'
-TOPIC_LDR = 'nodes/ldr'
+TOPIC_REQUEST_STATUS = "nodes/requestStatus"
+
 
 
 def on_connect(client, userdata, flags, rc):
     mqtt_client.subscribe(TOPIC_STATUS)
+    mqtt_client.subscribe(TOPIC_REQUEST_STATUS)
+    mqtt_client.subscribe(TOPIC_LED)
+    mqtt_client.subscribe(TOPIC_THRESHOLD)
 
 
 def on_message(client, userdata, message):
-    global latest_status
-    payload = message.payload.decode()
-    if payload == "getStatus":
-        mqtt_client.publish(TOPIC_STATUS, str(latest_status))  # Publish the latest_status as a string
-    else:
-        status = payload.split(',')
-        if len(status) == 6:
-            latest_status = {
-                "LED 1": {"state": int(status[0]), "threshold": int(status[1])},
-                "LED 2": {"state": int(status[2]), "threshold": int(status[3])},
-                "LED 3": {"state": int(status[4]), "threshold": int(status[5])},
-            }
-        else:
-            print("Invalid payload format:", payload)
-
+    print(message.payload.decode())  # Print received message payload
 
 
 mqtt_client.on_connect = on_connect
@@ -60,8 +42,8 @@ def index():
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
-    mqtt_client.publish(TOPIC_STATUS, "getStatus")
-    return jsonify(latest_status)
+    mqtt_client.publish(TOPIC_REQUEST_STATUS, "getStatus")
+    return 'OK', 200
 
 
 @app.route('/change_threshold', methods=['POST'])
